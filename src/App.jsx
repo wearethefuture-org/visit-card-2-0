3b2e194a3148b2e461f04388438d321003264c17
vipe4ka-visko4ka <pavel.makk.old@gmail.com>
2020-07-26 21:04:04 +0300

    Agency banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { ReactSVG } from 'react-svg';
 import './App.scss';
 
+const AgencyBanner = () => (
+  <div className="agency-banner">
+    <h1 className="agency-banner__title">We Are the Future</h1>
+    <ReactSVG src='/assets/svgs/logos/banner-logo.svg' className="agency-banner__logo" />
+    <ReactSVG src='/assets/svgs/scroll-down.svg' className="agency-banner__scroller" />
+  </div>
+);
+
 const App = () => (
-  <h1>Work</h1>
+<>
+  <AgencyBanner />
+</>
 );
 
 export default App;
