fb5da5aa53c4f8a609c8cec06bea9eb6826b4c6d
vipe4ka-visko4ka <pavel.makk.old@gmail.com>
2020-07-27 17:48:04 +0300

    Images

--- src/constants/developers.jsx
+++ src/constants/developers.jsx
@@ -1,33 +1,38 @@
 const DEVELOPERS = [
   {
-    img: 'https://i1.wp.com/novipel.com/wp-content/uploads/2018/03/christopher-campbell-28567-unsplash.jpg?ssl=1',
-    name: 'Valera Galushka',
-    profession: 'Strong Junior Web Full-Stack Developer'
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=635&q=80',
-    name: 'Vasya Pupkin',
+    img: '/assets/imgs/devs/ihor.png',
+    name: 'Ihor Zhariy',
     profession: 'Senior Web Full-Stack Developer'
   },
   {
-    img: 'https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-    name: 'Valerian Mustodotovich',
-    profession: 'Strong Middle Web Full-Stack Developer'
+    img: '/assets/imgs/devs/roma.jpg',
+    name: 'Roma Humenuk',
+    profession: 'Project manager'
   },
   {
-    img: 'https://images.unsplash.com/photo-1495366691023-cc4eadcc2d7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-    name: 'Galia Pomelo',
-    profession: 'Strong Junior Web Full-Stack Developer'
+    img: '/assets/imgs/devs/semen.png',
+    name: 'Semen Yevmenenko',
+    profession: 'Senior Web Front-End Developer'
   },
   {
-    img: 'https://images.unsplash.com/photo-1588571590812-47c47362b56a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-    name: 'Potap Kamenskyh',
-    profession: 'Strong Junior Web Full-Stack Developer'
+    img: '/assets/imgs/devs/maxim.png',
+    name: 'Maxim Polishchuk',
+    profession: 'Senior DevOps Engineer'
   },
   {
-    img: 'https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-    name: 'Anatoliy Fantazer',
-    profession: 'Strong Middle DevOps Engineer'
+    img: '/assets/imgs/devs/bogdan.png',
+    name: 'Bogdan Dereviaga',
+    profession: 'Senior Back-End Developer'
+  },
+  {
+    img: '/assets/imgs/devs/pavel.png',
+    name: 'Pavel Makukha',
+    profession: 'Middle Web Full-Stack Developer'
+  },
+  {
+    img: '/assets/imgs/devs/sergei.png',
+    name: 'Sergei ',
+    profession: 'Scrum master'
   }
 ];
 
